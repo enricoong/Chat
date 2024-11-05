8857793ac1df09e8e9e8f0cb4f2f291e702abe62
Enrico Negretto <negrettoenrico@gmail.com>
2024-11-05 12:56:36 +0100

    Added log4j and its test on main

--- src/main/java/itts/volterra/quintab/Main.java
+++ src/main/java/itts/volterra/quintab/Main.java
@@ -1,7 +1,13 @@
 package itts.volterra.quintab;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+   private static final Logger log = LogManager.getLogger(Main.class);
+
+   public static void main(String[] args) {
+      System.out.println("Hello world!");
+      log.debug("Test");
+   }
 }
