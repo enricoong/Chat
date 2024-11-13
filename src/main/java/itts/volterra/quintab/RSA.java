acc4416214e952df87bf2c402f64a2cb6291ded0
Enrico Negretto <negrettoenrico@gmail.com>
2024-11-13 10:10:46 +0100

    Changed final variable name to caps
    Took 17 minutes

--- src/main/java/itts/volterra/quintab/RSA.java
+++ src/main/java/itts/volterra/quintab/RSA.java
@@ -9,16 +9,16 @@ import java.security.SecureRandom;
 public class RSA {
    private static final Logger log = LogManager.getLogger(RSA.class);
    private static BigInteger n, d, e;
-   private final int bitLength = 1024;
+   private final int BIT_LENGTH = 1024;
 
    /**
     * Costruttore che mi genera i numeri necessari in futuro per criptare e decriptare.
     */
    public RSA(){
       SecureRandom random = new SecureRandom();
-      BigInteger p = BigInteger.probablePrime(bitLength / 2, random);
+      BigInteger p = BigInteger.probablePrime(BIT_LENGTH / 2, random);
       log.debug("Generated 'p' prime number: {}", p);
-      BigInteger q = BigInteger.probablePrime(bitLength / 2, random);
+      BigInteger q = BigInteger.probablePrime(BIT_LENGTH / 2, random);
       log.debug("Generated 'q' prime number: {}", q);
 
       //n = p * q
@@ -30,7 +30,7 @@ public class RSA {
       log.debug("Generated 'phi': {}", phi);
 
       //condizioni: 1 < e < phi(n) e gcd(e, phi(n)) = 1
-      e = BigInteger.probablePrime(bitLength / 2, random);
+      e = BigInteger.probablePrime(BIT_LENGTH / 2, random);
       while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0) {
          e = e.add(BigInteger.ONE);
       }
