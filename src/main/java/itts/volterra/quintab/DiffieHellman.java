27049c1b23233756ec39b527cf65fea5bbc192c5
Enrico Negretto <negrettoenrico@gmail.com>
2024-11-25 11:01:54 +0100

    Added comment explaination
    Signed-off-by: Enrico Negretto <negrettoenrico@gmail.com>

--- src/main/java/itts/volterra/quintab/DiffieHellman.java
+++ src/main/java/itts/volterra/quintab/DiffieHellman.java
@@ -3,6 +3,19 @@ package itts.volterra.quintab;
 import java.math.BigInteger;
 
 public class DiffieHellman {
+
+    /*
+    Alice e Bob si accordano di usare un numero primo p = 23 e la base g = 5.
+    Alice sceglie un numero segreto a = 6 e manda a Bob A = ga mod p
+        A = 56 mod 23 = 8
+    Bob sceglie l'intero segreto b = 15 e manda ad Alice B = gb mod p
+        B = 515 mod 23 = 19.
+    Alice calcola KA = (gb mod p)a mod p = Ba mod p
+        KA = 196 mod 23 = 2.
+    Bob calcola KB = (ga mod p)b mod p = Ab mod p
+        KB = 815 mod 23 = 2.
+     */
+
     BigInteger nPrimo, base;
 
     public DiffieHellman(BigInteger nPrimo, BigInteger base) {
