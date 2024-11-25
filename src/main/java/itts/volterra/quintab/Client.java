6f3f7123c714f9ff2ecca0912546d6fbe92fece4
Enrico Negretto <negrettoenrico@gmail.com>
2024-11-25 22:05:41 +0100

    Aggiungendo connessione di rete contemporaneamente a scambio delle chiavi
    Signed-off-by: Enrico Negretto <negrettoenrico@gmail.com>
    
    Took 1 hour 50 minutes

--- src/main/java/itts/volterra/quintab/Client.java
+++ src/main/java/itts/volterra/quintab/Client.java
@@ -8,43 +8,112 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.math.BigInteger;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
+import java.util.Scanner;
 
 public class Client implements Runnable{
 
     private static final Logger log = LogManager.getLogger(Client.class);
     private static BigInteger privateKey;   //chiave privata di questo client
     private BigInteger ky;                  //chiave da scambiare al pubblico
-    private BigInteger G, P;                // numeri primi
+    private static BigInteger G, P;                // numeri primi
+    private Scanner kbInput = new Scanner(System.in);
 
     @Override
     public void run() {
-        //connessione a altro client
+        log.debug("IP di questa macchina: {}", getIpOfCurrentMachine());
+        log.warn("Inserisci IP della macchina a cui connettersi >");
+        String userInsertedIP = kbInput.nextLine().trim();
+
+        connectToClient(userInsertedIP, 12345);
         runDiffieHellmanAlgorithm();
         //roba
     }
 
-    private static Socket connectToClient(String machineIP, int port){
+    /**
+     * Si connette a una macchina e ci comunica
+     *
+     * @param machineIP IP macchina
+     * @param port Porta di connessione
+     */
+    private static void connectToClient(String machineIP, int port){
         while (true){   //ciclo di ascolto
             try {
                 log.info("In attesa di connessione...");                //log attesa di connessione
                 Socket otherClient = new ServerSocket(port).accept();   //attendo richiesta di connessione, bloccante
                 log.info("Client connesso!");                           //log connessione del client
 
-                BufferedReader bR = new BufferedReader(new InputStreamReader(otherClient.getInputStream()));    //listener
-                String otherClientMessage = bR.readLine();              //ricevo e salvo messaggio da altro client
-                log.debug("Messaggio ricevuto: {}", otherClientMessage);
+                BufferedReader bR = new BufferedReader(new InputStreamReader(otherClient.getInputStream()));    //input stream
+                {
+                    boolean stop = false;
+                    while (!stop){  //fermo il ciclo solo in caso di richiesta dall'altro client
+                        String otherClientMessage = bR.readLine();      //ricevo e salvo messaggio da altro client
+                        log.debug("Messaggio ricevuto: {}", otherClientMessage);    //log ricezione
 
-                PrintWriter pW = new PrintWriter(otherClient.getOutputStream(), true);  //writer per scrivere
-                pW.println("Messaggio ricevuto!");  //invio conferma ricezione
+                        if (otherClientMessage.equals("END_CONNECTION")){   //altro client richiede terminazione connessione
+                            stop = true;
+                        } else {
+                            manageMessage(otherClientMessage, otherClient); //gestisco messaggio
+                        }
+                    }
+                }
 
+                bR.close();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
     }
 
+    /**
+     * Gestisce un messaggio ricevuto da un Socket
+     *
+     * @param message Messagio
+     * @param socket Socket
+     */
+    private static void manageMessage(String message, Socket socket){
+        switch (message.substring(0, 3)){
+            case "P--" -> {
+                setP(new BigInteger(message.substring(3).getBytes()));
+            }
+
+            case "G--" -> {
+                setG(new BigInteger(message.substring(3).getBytes()));
+            }
+        }
+    }
+
+    /**
+     * Scrive nell'output stream di un Socket
+     *
+     * @param client Socket
+     * @param message Messaggio da scrivere
+     * @throws IOException Cagati addosso
+     */
+    private void sendMessageToSocket(Socket client, String message) throws IOException {
+        PrintWriter pW = new PrintWriter(client.getOutputStream(), true);  //writer per scrivere
+        pW.println(message);        //invio conferma ricezione
+        pW.close();                 //chiudo stream
+    }
+
+    /**
+     * Mi dice l'IP della macchina attuale
+     *
+     * @return IP della macchina attuale
+     */
+    private String getIpOfCurrentMachine() {
+        try (final DatagramSocket datagramSocket = new DatagramSocket()){
+            datagramSocket.connect(InetAddress.getByName("1.1.1.1"), 12345);
+            return datagramSocket.getLocalAddress().getHostAddress();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;    //non dovrebbe arrivare qui ma ora non ho tempo per gestire bene le eccezioni
+    }
+
+    /**
+     * Metodo che richiama tutti i sotto-metodi che eseguono l'algoritmo di Diffie-Hellman
+     */
     private static void runDiffieHellmanAlgorithm(){
         //get from server G and P
 
@@ -53,11 +122,27 @@ public class Client implements Runnable{
         //now send key to other client
     }
 
+    /**
+     * Genera una chiave necessaria per l'algoritmo di Diffie-Hellman
+     *
+     * @param x Base
+     * @param y Potenza
+     * @param P Modulo
+     * @return Risultato
+     */
     private static BigInteger generateIntermediateKey(BigInteger x, BigInteger y, BigInteger P){
         return x.modPow(y, P);
     }
 
     private static BigInteger generatePrivateKey(BigInteger y){
         return y.modPow(privateKey, P);
+    }
+
+    public static void setP(BigInteger p) {
+        P = p;
+    }
+
+    public static void setG(BigInteger g) {
+        G = g;
     }
 }
--- src/main/java/itts/volterra/quintab/ClientSocket.java
+++ src/main/java/itts/volterra/quintab/ClientSocket.java
@@ -1,74 +1,74 @@
-package itts.volterra.quintab;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.ConnectException;
-import java.net.InetAddress;
-import java.net.Socket;
-import java.net.UnknownHostException;
-
-public class ClientSocket {
-    private final static int PORT = 12345;                      //porta
-    private static final Logger log = LogManager.getLogger(ClientSocket.class);
-    private static Socket client;                               //oggetto socket
-    private InetAddress host = InetAddress.getLocalHost();      //prendo ip macchina
-    private PrintWriter out;                                    //invia messaggio a server
-    private static BufferedReader in;                           //ricevi messaggio
-
-    public ClientSocket(String nome) throws UnknownHostException {
-        try {
-            client = new Socket(host.getHostName(), PORT);
-            log.debug("Server connesso");
-            out = new PrintWriter(client.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public ClientSocket(String nome, String hostName) throws UnknownHostException, ConnectException {
-        try {
-            client = new Socket(hostName, PORT);
-            log.debug("Server connesso");
-            out = new PrintWriter(client.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        } catch (ConnectException connectException){
-            throw connectException;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Mi permette di inviare un messaggio al server
-     *
-     * @param msg Messaggio da inviare
-     * @return Risposta ricevuta (è inizializzata a 'null')
-     */
-    public String sendMessage(String msg){
-        out.println(msg); //invia messaggio
-        String response = null;
-        try {
-            response = in.readLine();   //ritorna risposta
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-
-        //System.out.println("Risposta del server: " + response);   //debug
-
-        return response;
-    }
-
-    public static Socket getClient() {
-        return client;
-    }
-
-    public static BufferedReader getIn() {
-        return in;
-    }
-}
+//package itts.volterra.quintab;
+//
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.io.PrintWriter;
+//import java.net.ConnectException;
+//import java.net.InetAddress;
+//import java.net.Socket;
+//import java.net.UnknownHostException;
+//
+//public class ClientSocket {
+//    private final static int PORT = 12345;                      //porta
+//    private static final Logger log = LogManager.getLogger(ClientSocket.class);
+//    private static Socket client;                               //oggetto socket
+//    private InetAddress host = InetAddress.getLocalHost();      //prendo ip macchina
+//    private PrintWriter out;                                    //invia messaggio a server
+//    private static BufferedReader in;                           //ricevi messaggio
+//
+//    public ClientSocket(String nome) throws UnknownHostException {
+//        try {
+//            client = new Socket(host.getHostName(), PORT);
+//            log.debug("Server connesso");
+//            out = new PrintWriter(client.getOutputStream(), true);
+//            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public ClientSocket(String nome, String hostName) throws UnknownHostException, ConnectException {
+//        try {
+//            client = new Socket(hostName, PORT);
+//            log.debug("Server connesso");
+//            out = new PrintWriter(client.getOutputStream(), true);
+//            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+//        } catch (ConnectException connectException){
+//            throw connectException;
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    /**
+//     * Mi permette di inviare un messaggio al server
+//     *
+//     * @param msg Messaggio da inviare
+//     * @return Risposta ricevuta (è inizializzata a 'null')
+//     */
+//    public String sendMessage(String msg){
+//        out.println(msg); //invia messaggio
+//        String response = null;
+//        try {
+//            response = in.readLine();   //ritorna risposta
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+//
+//        //System.out.println("Risposta del server: " + response);   //debug
+//
+//        return response;
+//    }
+//
+//    public static Socket getClient() {
+//        return client;
+//    }
+//
+//    public static BufferedReader getIn() {
+//        return in;
+//    }
+//}
