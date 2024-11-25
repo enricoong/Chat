1b50b87b5c449f51c279796448de9798cf306b74
Enrico Negretto <negrettoenrico@gmail.com>
2024-11-25 11:00:11 +0100

    Back to original encryption, started to add DiffieHellman
    Signed-off-by: Enrico Negretto <negrettoenrico@gmail.com>

--- src/main/java/itts/volterra/quintab/DiffieHellman.java
+++ src/main/java/itts/volterra/quintab/DiffieHellman.java
@@ -1,5 +1,12 @@
 package itts.volterra.quintab;
 
+import java.math.BigInteger;
+
 public class DiffieHellman {
-    //TODO
+    BigInteger nPrimo, base;
+
+    public DiffieHellman(BigInteger nPrimo, BigInteger base) {
+        this.nPrimo = nPrimo;
+        this.base = base;
+    }
 }
